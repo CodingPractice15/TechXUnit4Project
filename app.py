0cbda3ae7d388f37de6c456d041db527643ae506
Benjamin Hansen <bhansen@techexchange.in>
2022-04-14 17:42:08 +0000

    added function to return list of state from file for new report

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from flask import render_template
 from flask import request, redirect
 from flask_pymongo import PyMongo
 import secrets
+from model import *
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -47,7 +48,13 @@ def index():
 
 @app.route('/newreport',methods=['GET', 'POST'])
 def newreport():
-    return render_template('newreport.html')
+    if request.method == "GET":
+        #calling method from model to get list of US states
+        list_state = get_list_states()
+        
+        return render_template('newreport.html',list_state=list_state)
+    else:
+        return "Post"
 
 # SignUp Route
 @app.route('/signup',methods=['GET', 'POST'])
--- model.py
+++ model.py
@@ -11,3 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+def get_list_states():
+    lst_states = []
+    with open("state.txt",'r') as f:
+        lines_list = f.readlines()
+        for state in lines_list:
+            lst_states.append(state.strip())
+    return lst_states
+            
+
+
